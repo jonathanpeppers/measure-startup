3fec32937973924c54c4a641515f3b23e701d706
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-08-11 15:10:54 -0500

    Kind of works, OnExited doesn't fire
    This works as expected:
    
        > dotnet run -- dotnet help
        00:00:00.0189146
    
    It runs `dotnet` and reports when the word `help` is printed.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Diagnostics;
+
+string processToRun = "dotnet";
+string stdoutText = "asdf";
+
+var psi = new ProcessStartInfo(processToRun)
+{
+	RedirectStandardError = true,
+	RedirectStandardOutput = true,
+};
+using var p = new Process { StartInfo = psi };
+
+bool exited = false;
+var resetEvent = new ManualResetEvent(false);
+p.ErrorDataReceived += OnDataReceived;
+p.OutputDataReceived += OnDataReceived;
+p.Exited += OnExited;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+p.Start();
+p.BeginErrorReadLine();
+p.BeginOutputReadLine();
+
+resetEvent.WaitOne();
+if (exited)
+{
+	throw new Exception($"Exited before '{stdoutText}' was printed.");
+}
+stopwatch.Stop();
+
+Console.WriteLine(stopwatch.Elapsed.ToString());
+
+void OnDataReceived(object sender, DataReceivedEventArgs e)
+{
+	if (!string.IsNullOrEmpty(e.Data) && e.Data.IndexOf(stdoutText, StringComparison.OrdinalIgnoreCase) != -1)
+	{
+		resetEvent.Set();
+	}
+}
+
+void OnExited(object? sender, EventArgs e)
+{
+	exited = true;
+	resetEvent.Set();
+}
